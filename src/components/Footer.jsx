781e5b806b9c7eb009e4b2dcec3df8573636208d
Sacha-Marciano <Momarciano26@gmail.com>
2025-12-02 21:53:30 +0200

    initial commit

---
+++ src/components/Footer.jsx
@@ -0,0 +1,12 @@
+import "../styles/Footer.css";
+
+function Footer() {
+  return (
+    <div className="footer">
+      <p className="footer_paragraph">© All Rights Reserved</p>
+      <p className="footer_paragraph">Sacha Marciano</p>
+    </div>
+  );
+}
+
+export default Footer;
