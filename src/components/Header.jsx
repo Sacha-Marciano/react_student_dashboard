603b6bc7e19fed537a1733af9bfd5672a895c36b
Sacha-Marciano <Momarciano26@gmail.com>
2025-12-02 21:59:48 +0200

    change title

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,7 +5,7 @@ import "../styles/Header.css";
 function Header() {
   return (
     <div className="header">
-      <h1 className="title">Students Dashboard</h1>
+      <h1 className="title">Dashboard</h1>
       <img src={logo} alt="Logo" width={104} height={104} />
     </div>
   );
